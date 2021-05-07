a65caedd01d61f95fd836814cd781755e7af7c2f
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-05-07 09:21:23 +0300

    Add the initial project.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(css|less|scss|sass)$': '<rootDir>/.jest/identity-obj-proxy-esm.js',
+  },
+};
