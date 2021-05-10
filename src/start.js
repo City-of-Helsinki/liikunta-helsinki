56c89696e119356b42d10c4df4ab561067e085f5
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-05-10 13:37:47 +0300

    Remove unused server.js

--- src/start.js
+++ src/start.js
@@ -1,7 +1,4 @@
 /* eslint-disable */
-// This line is needed so we can use import statements in server.js file
-// https://github.com/standard-things/esm#getting-started
-require = require("esm")(module);
 
 // HDS injects component styles to document. Document needs to be initialized to global object
 // see pages/_document.ts, it takes the generated styles from document.head and renders them
