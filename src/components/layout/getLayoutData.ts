d69b275aa0cf572480845948b358ea6eee0c095e
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-05-10 15:56:05 +0300

    Don't accept language if it's not supported

--- src/components/layout/getLayoutData.ts
+++ src/components/layout/getLayoutData.ts
@@ -14,9 +14,18 @@ async function getLayoutData(
 ): Promise<LayoutData> {
   const sortedMenuItems = [...menuItems].sort((a, b) => a.order - b.order);
   const navigationItems = sortedMenuItems.map(({ order, ...rest }) => rest);
+  const allLanguages = await LanguageApi.get();
+  // NextJS uses locales as is in the slug. The headless CMS has a locale and
+  // a slug field. The slug field is meant to be used in urls. To be able to do
+  // so, we have to provide the slug field value for NextJS when configuring its
+  // i18n module. That's why we are trying to find the slug field from NextJS
+  // locales.
+  const supportedLanguages = allLanguages.filter(({ slug }) =>
+    context.locales.includes(slug)
+  );
 
   return {
-    languages: await LanguageApi.get(),
+    languages: supportedLanguages,
     navigationItems,
   };
 }
