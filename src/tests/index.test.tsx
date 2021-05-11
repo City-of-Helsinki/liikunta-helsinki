9558a2b0af94f0481e5e438d9fe69fece4586933
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-05-11 08:55:32 +0300

    Fix app smoke test

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -4,7 +4,31 @@ import App from "../pages/index";
 
 describe("App", () => {
   it("renders without crashing", () => {
-    render(<App />);
+    render(
+      <App
+        page={{
+          layout: {
+            navigationItems: [
+              {
+                id: "1",
+                path: "/haku",
+                target: "",
+                title: "Haku",
+                url: "/haku",
+              },
+              {
+                id: "3",
+                path: "/liikuntatunnit",
+                target: "",
+                title: "Liikuntatunnit",
+                url: "/liikuntatunnit",
+              },
+            ],
+            languages: [],
+          },
+        }}
+      />
+    );
     expect(screen.getByText("Liikunta-Helsinki")).toBeInTheDocument();
   });
 });
