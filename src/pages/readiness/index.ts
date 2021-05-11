07379554df4767da08a8345a31f3618ff90b87ee
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-05-11 09:05:17 +0300

    Move healthz and readiness into api endpoints

--- src/pages/readiness/index.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-function readiness() {}
-
-export const getServerSideProps = async (context) => {
-  context.res.end("OK");
-  return { props: {} };
-};
-
-export default readiness;
