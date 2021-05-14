34ac9874211478d64056cc0169adb1b09cbc054c
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-05-14 08:30:33 +0300

    Add link to all collections to collection section

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import { IconAngleRight } from "hds-react";
 
 import Text from "../text/Text";
 import styles from "./section.module.scss";
@@ -6,12 +8,30 @@ import styles from "./section.module.scss";
 type Props = {
   title: string;
   children: React.ReactNode;
+  cta?: {
+    label: string;
+    href: string;
+  };
 };
 
-function Section({ title, children }: Props) {
+function Section({ title, children, cta }: Props) {
+  const titleComponent = <Text variant="h2">{title}</Text>;
+
   return (
     <section className={styles.section}>
-      <Text variant="h2">{title}</Text>
+      {!cta && titleComponent}
+      {cta && (
+        <header className={styles.sectionHeader}>
+          {titleComponent}
+          <Link href={cta.href}>
+            {/* <Link /> applies href */}
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a>
+              {cta.label} <IconAngleRight size="s" />
+            </a>
+          </Link>
+        </header>
+      )}
       {children}
     </section>
   );
