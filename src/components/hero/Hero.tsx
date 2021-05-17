b09e6c70136af4fe1ef63f05205c001ce5dfa668
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-05-17 13:06:54 +0300

    Code style improvements.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,6 +1,7 @@
-import { Koros, Button } from "hds-react";
+import { Koros } from "hds-react";
 import Link from "next/link";
 
+import Text from "../text/Text";
 import { LandingPage } from "../../types";
 import styles from "./hero.module.scss";
 
@@ -11,10 +12,16 @@ function Hero({ title, link, desktopImage }: LandingPage) {
       style={{ backgroundImage: `url(${desktopImage.uri})` }}
     >
       <div className={styles.box}>
-        <p>Kokosimme yhteen</p>
-        <h1>{title}</h1>
-        <Link href={link}>
-          <Button className={styles.button}>Katso vinkit</Button>
+        <Text variant="body" className={styles.boxHelper}>
+          Kokosimme yhteen
+        </Text>
+        <Text variant="h1" className={styles.boxTitle}>
+          {title}
+        </Text>
+        <Link href={link} passHref>
+          <a href={link} className={styles.link}>
+            Katso vinkit
+          </a>
         </Link>
       </div>
       <div className={styles.korosBlock}>
