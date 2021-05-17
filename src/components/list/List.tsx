83b21b1620daf8bcc03641aa136f8715d072d4d8
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-05-17 21:09:54 +0300

    Use type that does not need configuring

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -4,16 +4,14 @@ import classNames from "classnames";
 import styles from "./list.module.scss";
 
 type Props = {
-  // eslint-disable-next-line no-undef
-  items: JSX.Element[];
+  items: React.ReactElement[];
   variant?: "default" | "collection-grid";
 };
 
 function List({ items, variant = "default" }: Props) {
   return (
     <ul className={classNames(styles.list, styles[variant])}>
-      {/* eslint-disable-next-line no-undef */}
-      {items.map((node: JSX.Element) => (
+      {items.map((node: React.ReactElement) => (
         <li key={node.key} className={styles.item}>
           {node}
         </li>
