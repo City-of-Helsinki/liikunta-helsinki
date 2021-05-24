97192895585559b8182eca0c94f6d547594d7ca4
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-05-24 10:43:55 +0300

    Remove all un-necessary filters.

--- src/components/search/searchPageSearchForm/SearchPageSearchForm.tsx
+++ src/components/search/searchPageSearchForm/SearchPageSearchForm.tsx
@@ -1,19 +1,6 @@
-import {
-  Tag,
-  Button,
-  TextInput,
-  Select,
-  Checkbox,
-  IconSearch,
-} from "hds-react";
+import { Button, TextInput, IconSearch } from "hds-react";
 
 import Text from "../../text/Text";
-import {
-  mockSportTypes,
-  mockArea,
-  mockPrice,
-  mockCategory,
-} from "./tmp/mockedData";
 import styles from "./searchPageSearchForm.module.scss";
 
 function SearchPageSearchForm() {
@@ -25,6 +12,8 @@ function SearchPageSearchForm() {
       <Text variant="h1">Mitä etsit?</Text>
       <form role="search" className={styles.form} onSubmit={handleSubmit}>
         <TextInput
+          label="Haku"
+          hideLabel
           placeholder="Kirjoita hakusana, esim. uimahalli tai jooga"
           id="text"
           name="text"
@@ -32,28 +21,6 @@ function SearchPageSearchForm() {
         >
           <IconSearch aria-hidden="true" />
         </TextInput>
-        <Select
-          label=""
-          id="place"
-          placeholder="Valitse paikka"
-          options={mockCategory}
-        />
-        <Select
-          label=""
-          id="sport"
-          placeholder="Valitse liikuntamuoto/laji"
-          options={mockSportTypes}
-        />
-        <Select label="" placeholder="Valitse alue" options={mockArea} />
-        <Select label="" placeholder="Valitse hinta" options={mockPrice} />
-
-        <Checkbox id="Box " label="Sopiva liikuntarajoitteisille" />
-        <Checkbox id="Box " label="Lähimmät paikat ensin" />
-        <Checkbox id="Box " label="Näytä vain ilmaiset" />
-        <Checkbox id="Box " label="Avoinna nyt" />
-        <div>
-          <Tag>Test</Tag>
-        </div>
         <Button type="submit">Hae</Button>
       </form>
     </div>
