7218f5dc708e2f483af437d83128e537d8d0de1b
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-06-07 08:46:21 +0300

    Hide content until first client render
    This should be considered as a temporary hack.
    
    During server side renders, HDS styles can't be applied because there's
    no document object. HDS attempts to embed styles into the head of the
    document.
    
    This causes a flash of unstyled content, which creates friction during
    demos. As a workaround for now, we are postponing the first visible
    render of the page to stop the untyled content from being showed.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 import { useCmsApollo } from "../api/cmsApolloClient";
 import AppMeta from "../components/meta/AppMeta";
@@ -7,6 +8,22 @@ import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const cmsApolloClient = useCmsApollo(pageProps.initialApolloState);
+
+  // Unset hidden visibility that was applied to hide the first server render
+  // that does not include styles from HDS. HDS applies styling by injecting
+  // style tags into the head. This requires the existence of a document object.
+  // The document object does not exist during server side renders.
+  // TODO: Remove this hackfix to ensure that pre-rendered pages'
+  //       SEO performance is not impacted.
+  useEffect(() => {
+    setTimeout(() => {
+      const body = document?.body;
+
+      if (body) {
+        body.style.visibility = "unset";
+      }
+    }, 10);
+  }, []);
 
   return (
     <ApolloProvider client={cmsApolloClient}>
