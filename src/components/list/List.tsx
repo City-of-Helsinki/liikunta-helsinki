2e5cc42f4212ff7ddb9846a3caa5090e02850158
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-06-09 14:20:39 +0300

    Add following events to details page

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -5,12 +5,23 @@ import styles from "./list.module.scss";
 
 type Props = {
   items: React.ReactElement[];
-  variant?: "default" | "collection-grid" | "tight" | "searchResult";
+  variant?:
+    | "default"
+    | "collection-grid"
+    | "tight"
+    | "searchResult"
+    | "columns-3";
 };
 
 function List({ items, variant = "default" }: Props) {
   return (
-    <ul className={classNames(styles.list, styles[variant])}>
+    <ul
+      className={classNames({
+        [styles.list]: variant !== "columns-3",
+        [styles[variant]]: variant !== "columns-3",
+        [styles["columns-3"]]: variant === "columns-3",
+      })}
+    >
       {items.map((node: React.ReactElement) => (
         <li key={node.key} className={styles.item}>
           {node}
