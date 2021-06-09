903b15ec84cec0786f03e1f2629786019d45fe81
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-06-09 09:22:32 +0300

    Fix broken test.

--- src/util/api/__tests__/getSourceUrl.test.ts
+++ src/util/api/__tests__/getSourceUrl.test.ts
@@ -3,15 +3,11 @@ import getSourceUrl from "../getSourceUrl";
 it("It returns correct url based on source", () => {
   const linkedURL = getSourceUrl("linked", "61903");
   const tprekURL = getSourceUrl("tprek", "61903");
-  const defaultURL = getSourceUrl("asd", "61903");
 
   expect(linkedURL).toEqual(
     "https://api.hel.fi/linkedevents/v1/place/tprek:61903/"
   );
   expect(tprekURL).toEqual(
-    "https://www.hel.fi/palvelukarttaws/rest/v4/unit/61903/"
-  );
-  expect(defaultURL).toEqual(
     "https://www.hel.fi/palvelukarttaws/rest/v4/unit/61903/"
   );
 });
