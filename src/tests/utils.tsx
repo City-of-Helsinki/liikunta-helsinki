15e6b902c85d5bce8ad04e3cf8b9f8e6403a1524
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-06-16 12:55:33 +0300

    Fix entity details test

--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -1,17 +1,21 @@
 import React, { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { MockedResponse } from "@apollo/client/testing";
+import { NextRouter } from "next/router";
 
 import TestProviders from "./TestProviders";
 
 const customRender = (
   ui: ReactElement,
   mocks?: MockedResponse[],
+  router?: Partial<NextRouter>,
   options?: RenderOptions
 ) =>
   render(ui, {
     wrapper: ({ children }) => (
-      <TestProviders mocks={mocks}>{children}</TestProviders>
+      <TestProviders mocks={mocks} router={router}>
+        {children}
+      </TestProviders>
     ),
     ...options,
   });
