aec703b7cce5280fadde9d879841850e1c8ce15b
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-08-18 14:39:53 +0300

    Define list render logic once

--- src/widgets/searchEvents/SearchEvents.tsx
+++ src/widgets/searchEvents/SearchEvents.tsx
@@ -1,12 +1,10 @@
 import { useQuery, gql } from "@apollo/client";
-import { LoadingSpinner } from "hds-react";
 
 import { useNextApiApolloClient } from "../../client/nextApiApolloClient";
 import getEventsAsItems from "../../util/events/getEventsAsItems";
 import eventFragment from "../../util/events/eventFragment";
 import useRouter from "../../domain/i18n/router/useRouter";
-import List from "../../components/list/List";
-import CondensedCard from "../../components/card/CondensedCard";
+import { ItemsPromiseObject } from "../../types";
 
 const SEARCH_EVENTS_QUERY = gql`
   query SearchEventsQuery($query: EventQuery!) {
@@ -30,9 +28,10 @@ function getEventQuery(url: string) {
 
 type Props = {
   url: string;
+  render: (renderProps: ItemsPromiseObject) => JSX.Element;
 };
 
-export default function SearchEventsSection({ url }: Props) {
+export default function SearchEventsSection({ url, render }: Props) {
   const nextApiApolloClient = useNextApiApolloClient();
   const router = useRouter();
   const locale = router.locale ?? router.defaultLocale;
@@ -48,28 +47,5 @@ export default function SearchEventsSection({ url }: Props) {
     fetchPolicy: "cache-and-network",
   });
 
-  if (loading) {
-    return <LoadingSpinner />;
-  }
-
-  // In case of an error, silently fail.
-  if (error) {
-    return null;
-  }
-
-  const eventItems = getEventsAsItems(data?.events);
-
-  // In case there are no events
-  if (eventItems.length === 0) {
-    return null;
-  }
-
-  return (
-    <List
-      variant="grid-2"
-      items={eventItems.map((item) => (
-        <CondensedCard key={item.id} {...item} />
-      ))}
-    />
-  );
+  return render({ loading, error, items: getEventsAsItems(data?.events) });
 }
