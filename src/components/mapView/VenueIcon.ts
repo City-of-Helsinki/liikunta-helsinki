a95a7087ddbc7ba48ac7d9bca495702e03a9faa6
Santtu Tuovinen <santtu.tuovinen@hel.fi>
2021-08-31 11:08:19 +0300

    Make map view more accessible.

--- src/components/mapView/VenueIcon.ts
+++ src/components/mapView/VenueIcon.ts
@@ -1,6 +1,18 @@
 import L from "leaflet";
 
-const venueIcon = new L.Icon({
+const MapAriaHiddenIcon = L.Icon.extend({
+  _setIconStyles(img: HTMLImageElement, name: string) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    L.Icon.prototype._setIconStyles.call(this, img, name);
+
+    img.setAttribute("aria-hidden", "true");
+  },
+});
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const venueIcon = new MapAriaHiddenIcon({
   iconUrl: "/images/venue-icon.svg",
   iconRetinaUrl: "/images/venue-icon.svg",
   iconAnchor: null,
