f6c1875dd72a0314f7eb9a4613bd3291d375f264
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-09-07 11:19:01 +0300

    Add clusters to map

--- src/components/mapView/MapView.tsx
+++ src/components/mapView/MapView.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 import { useTranslation } from "next-i18next";
 import "leaflet/dist/leaflet.css";
+import "react-leaflet-markercluster/dist/styles.min.css";
 
-import venueIcon from "./VenueIcon";
-import Text from "../text/Text";
-import styles from "./mapView.module.scss";
 import { MapItem } from "../../types";
+import Text from "../text/Text";
+import venueIcon from "./VenueIcon";
 import {
   BOUNDARIES,
   DEFAULT_POSITION,
@@ -16,6 +18,21 @@ import {
   MIN_ZOOM,
   TILE_URL,
 } from "./mapConstants";
+import styles from "./mapView.module.scss";
+
+function createCustomClusterIcon(cluster) {
+  return L.divIcon({
+    html: `<div><span>${cluster.getChildCount()}</span></div>`,
+    className: [
+      "leaflet-marker-icon",
+      "marker-cluster marker-cluster-small",
+      "leaflet-zoom-animated",
+      "leaflet-interactive",
+      styles.clusterIcon,
+    ].join(" "),
+    iconSize: L.point(40, 40, true),
+  });
+}
 
 type Props = {
   items: MapItem[];
@@ -45,25 +62,31 @@ function MapView({ items = [] }: Props) {
           )}`}
           url={TILE_URL}
         />
-        {items?.map((item) => {
-          if (!item.location) {
-            return null;
-          }
+        <MarkerClusterGroup
+          chunkedLoading
+          showCoverageOnHover={false}
+          iconCreateFunction={createCustomClusterIcon}
+        >
+          {items?.map((item) => {
+            if (!item.location) {
+              return null;
+            }
 
-          return (
-            <Marker
-              key={item.id}
-              position={[item.location[1], item.location[0]]}
-              icon={venueIcon}
-              keyboard={false}
-            >
-              <Popup className={styles.popup}>
-                <Text variant="body">{item.title}</Text>
-                <Link href={item.href}>{t("go_to_venue")}</Link>
-              </Popup>
-            </Marker>
-          );
-        })}
+            return (
+              <Marker
+                key={item.id}
+                position={[item.location[1], item.location[0]]}
+                icon={venueIcon}
+                keyboard={false}
+              >
+                <Popup className={styles.popup}>
+                  <Text variant="body">{item.title}</Text>
+                  <Link href={item.href}>{t("go_to_venue")}</Link>
+                </Popup>
+              </Marker>
+            );
+          })}
+        </MarkerClusterGroup>
       </MapContainer>
     </div>
   );
