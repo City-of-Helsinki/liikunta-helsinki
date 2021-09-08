e7e9b2fc43d296ce2ca0cb9eefd283614ead12d5
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-09-08 07:49:54 +0300

    Add selected filter toggles

--- src/components/search/searchPageSearchForm/SearchPageSearchForm.tsx
+++ src/components/search/searchPageSearchForm/SearchPageSearchForm.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Button, IconSearch } from "hds-react";
+import { Button, IconSearch, IconCross, LoadingSpinner } from "hds-react";
 import { gql, useLazyQuery } from "@apollo/client";
 import debounce from "lodash/debounce";
 import { useTranslation } from "next-i18next";
@@ -18,6 +18,9 @@ import SuggestionInput, {
 } from "../../suggestionInput/SuggestionInput";
 import { getUnifiedSearchLanguage } from "../../../client/utils";
 import styles from "./searchPageSearchForm.module.scss";
+import Keyword from "../../keyword/Keyword";
+import useAdministrativeDivisions from "../../../widgets/administrativeDivisionDropdown/useAdministrativeDivisions";
+import getTranslation from "../../../util/getTranslation";
 
 const SUGGESTION_QUERY = gql`
   query SuggestionQuery($prefix: String, $language: UnifiedSearchLanguage!) {
@@ -52,6 +55,7 @@ function SearchPageSearchForm({ showTitle = true, searchRoute }: Props) {
   const [findSuggestions, { data }] = useLazyQuery(SUGGESTION_QUERY, {
     client: searchApolloClient,
   });
+  const administrativeDivisionsQuery = useAdministrativeDivisions();
   const debouncedFindSuggestions = useRef(debounce(findSuggestions, 100));
 
   const doSearch = (query: UnifiedSearchParameters) => {
@@ -91,6 +95,52 @@ function SearchPageSearchForm({ showTitle = true, searchRoute }: Props) {
     setAdministrativeDivisionId(administrativeDivisionId);
   };
 
+  const getQueryParametersWithout = (parameterName: string) => {
+    const nextParameters = Object.entries(unifiedSearchParams).reduce(
+      (acc, [key, value]) => {
+        if (key === parameterName) {
+          return acc;
+        }
+
+        return {
+          ...acc,
+          [key]: value,
+        };
+      },
+      {}
+    );
+
+    return new URLSearchParams(nextParameters).toString();
+  };
+
+  const getSearchParameterLabel = (
+    key: string,
+    value: string | number
+  ): string | JSX.Element => {
+    if (key === "administrativeDivisionId") {
+      if (administrativeDivisionsQuery.loading) {
+        return <LoadingSpinner />;
+      }
+
+      const divisionData =
+        administrativeDivisionsQuery.data?.administrativeDivisions?.find(
+          (division) => division.id === value
+        );
+
+      if (!divisionData) {
+        return "Could not find division name";
+      }
+
+      return getTranslation(divisionData.name, router.locale);
+    }
+
+    if (typeof value === "string") {
+      return value;
+    }
+
+    return value.toString();
+  };
+
   const suggestions =
     data?.unifiedSearchCompletionSuggestions?.suggestions ?? [];
 
@@ -122,6 +172,22 @@ function SearchPageSearchForm({ showTitle = true, searchRoute }: Props) {
           onChange={handleAdminDivisionChange}
           value={administrativeDivisionId || ""}
         />
+        <div className={styles.searchAsFilters}>
+          {Object.entries(unifiedSearchParams).map(([key, value]) => (
+            <Keyword
+              key={key}
+              color="black"
+              icon={IconCross}
+              aria-label={`${t(
+                "remove_filter_aria_label"
+              )}: ${getSearchParameterLabel(key, value)}`}
+              keyword={getSearchParameterLabel(key, value)}
+              href={{
+                search: getQueryParametersWithout(key),
+              }}
+            />
+          ))}
+        </div>
         <Button
           type="submit"
           className={styles.submitButton}
