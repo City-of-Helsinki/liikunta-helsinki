23a90a69a8317d3057534a78d3c25644a0e8b240
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-09-08 16:28:40 +0300

    Add coverage for Navigation component

---
+++ src/components/navigation/tests/navigation.test.js
@@ -0,0 +1,27 @@
+import { render } from "../../../tests/utils";
+import Navigation from "../Navigation";
+
+describe("<Navigation />", () => {
+  it("should render correctly", () => {
+    const { container } = render(
+      <Navigation
+        navigationItems={[
+          {
+            id: "1",
+            label: "Test",
+            title: "Test",
+            url: "/url",
+          },
+        ]}
+        languages={[
+          {
+            id: "1",
+            name: "Suomeksi",
+            slug: "fi",
+          },
+        ]}
+      />
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
