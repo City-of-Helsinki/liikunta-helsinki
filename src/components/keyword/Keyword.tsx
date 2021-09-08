70ebb96fa519b455a41897f1505022eab92d78f1
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-09-08 07:49:54 +0300

    Add selected filter toggles

--- src/components/keyword/Keyword.tsx
+++ src/components/keyword/Keyword.tsx
@@ -8,10 +8,14 @@ import styles from "./keyword.module.scss";
 
 type Props = {
   blackOnMobile?: boolean;
-  color?: "engelLight50" | "tramLight20";
+  color?: "engelLight50" | "tramLight20" | "black";
   hideOnMobile?: boolean;
   keyword: string | JSX.Element;
   href: string | UrlObject;
+  icon?: React.ComponentType<{
+    className?: string;
+  }>;
+  "aria-label"?: string;
 };
 
 const Keyword = ({
@@ -20,16 +24,21 @@ const Keyword = ({
   hideOnMobile,
   keyword,
   href,
+  icon: Icon,
+  ...rest
 }: Props) => {
   return (
     <Link href={href}>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a
+        {...rest}
         className={classNames(styles.keyword, color && styles[color], {
           [styles.blackOnMobile]: blackOnMobile,
           [styles.hideOnMobile]: hideOnMobile,
+          [styles.withIcon]: Icon,
         })}
       >
+        {Icon && <Icon className={styles.icon} />}
         {keyword}
       </a>
     </Link>
--- src/widgets/administrativeDivisionDropdown/AdministrativeDivisionDropdown.tsx
+++ src/widgets/administrativeDivisionDropdown/AdministrativeDivisionDropdown.tsx
@@ -1,82 +1,11 @@
-import { gql, useQuery } from "@apollo/client";
 import { IconLocation } from "hds-react";
 import { useTranslation } from "next-i18next";
 
-import { LocalizedString, Option } from "../../types";
-import { Locale } from "../../config";
-import searchApolloClient from "../../client/searchApolloClient";
+import { Option } from "../../types";
 import Combobox from "../../components/combobox/Combobox";
 import useRouter from "../../domain/i18n/router/useRouter";
 import getTranslation from "../../util/getTranslation";
-
-type AdministrativeDivision = {
-  id: string;
-  type: string;
-  name: LocalizedString;
-};
-
-const ADMINISTRATIVE_DIVISION_QUERY = gql`
-  {
-    administrativeDivisions {
-      id
-      type
-      name {
-        fi
-        sv
-        en
-      }
-    }
-  }
-`;
-
-function filterNeighborhoods(
-  administrativeDivisions: AdministrativeDivision[]
-) {
-  if (!administrativeDivisions) {
-    return administrativeDivisions;
-  }
-
-  return administrativeDivisions.filter(
-    (division) => division.type === "neighborhood"
-  );
-}
-
-function sortAdministrativeDivisionsAlphabetically(
-  administrativeDivisions: AdministrativeDivision[],
-  locale: Locale
-) {
-  if (!administrativeDivisions) {
-    return administrativeDivisions;
-  }
-
-  const sorted = [...administrativeDivisions];
-
-  sorted.sort((a, b) => {
-    const aValue = getTranslation(a.name, locale);
-    const bValue = getTranslation(b.name, locale);
-
-    return aValue.toLowerCase().localeCompare(bValue.toLowerCase());
-  });
-
-  return sorted;
-}
-
-function useAdministrativeDivisions() {
-  const { locale } = useRouter();
-  const { data, ...delegated } = useQuery(ADMINISTRATIVE_DIVISION_QUERY, {
-    client: searchApolloClient,
-  });
-
-  return {
-    data: {
-      administrativeDivisions: sortAdministrativeDivisionsAlphabetically(
-        filterNeighborhoods(data?.administrativeDivisions),
-        locale
-      ),
-    },
-    ...delegated,
-  };
-}
+import useAdministrativeDivisions from "./useAdministrativeDivisions";
 
 type Props = {
   label?: string;
