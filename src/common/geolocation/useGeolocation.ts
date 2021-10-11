ef19040069fb5b68e6bb960c041584f385c16547
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-10-11 15:29:36 +0300

    Fix double warning

--- src/common/geolocation/useGeolocation.ts
+++ src/common/geolocation/useGeolocation.ts
@@ -7,16 +7,17 @@ type Config = {
 };
 
 export default function useGeolocation({ skip = false }: Config = {}) {
-  const { resolve, called, coordinates, ...delegated } =
+  const { resolve, called, coordinates, loading, error, ...delegated } =
     useGeolocationContext();
 
   useEffect(() => {
     // Defer resolving under not skipped
     // Don't resolve again if we have resolved before and found a geolocation
-    if (!skip && !called && !coordinates) {
+    // Don't resolve if the previous resolve is ongoing
+    if (!skip && !called && coordinates === undefined && !loading) {
       resolve();
     }
-  }, [resolve, skip, called, coordinates]);
+  }, [resolve, skip, called, coordinates, loading, error]);
 
-  return { resolve, called, coordinates, ...delegated };
+  return { resolve, called, coordinates, loading, ...delegated };
 }
