1cd8f078a2a9f6da8998e14775a4981d6d09cd12
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2021-10-12 09:59:42 +0300

    Translate close toast label

--- src/common/utils/sendToast.tsx
+++ src/common/utils/sendToast.tsx
@@ -1,23 +1,47 @@
-import { toast } from "react-toastify";
+import { toast, ToastContentProps } from "react-toastify";
 import { Notification, NotificationType, NotificationProps } from "hds-react";
+import { useTranslation } from "react-i18next";
 
-export default function sendToast(
-  title: string,
-  message: string,
-  type: NotificationType,
-  options?: Omit<Partial<NotificationProps>, "size">
-) {
-  toast(({ closeToast }) => (
+type NotificationOptions = Omit<Partial<NotificationProps>, "size">;
+
+type ToastProps = ToastContentProps & {
+  title: string;
+  message: string;
+  type: NotificationType;
+  options?: NotificationOptions;
+};
+
+function Toast({ closeToast, title, message, type, options }: ToastProps) {
+  const { t } = useTranslation("toast");
+
+  return (
     <Notification
       label={title}
       position="top-right"
       dismissible
-      closeButtonLabelText="Close toast"
+      closeButtonLabelText={t("close")}
       type={type}
       onClose={closeToast}
       {...options}
     >
       {message}
     </Notification>
+  );
+}
+
+export default function sendToast(
+  title: string,
+  message: string,
+  type: NotificationType,
+  options?: NotificationOptions
+) {
+  toast((content) => (
+    <Toast
+      {...content}
+      title={title}
+      message={message}
+      type={type}
+      options={options}
+    />
   ));
 }
