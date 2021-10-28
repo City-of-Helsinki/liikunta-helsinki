34d2719e3611785783d0d0d2a5aa0939baf4e3b1
Santeri Rusila <santtu_1993@hotmail.com>
2021-10-28 13:24:17 +0300

    update testing related libraries

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,16 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    "\\.(css|less|scss|sass)$": "<rootDir>/.jest/identity-obj-proxy-esm.js",
   },
   transformIgnorePatterns: [
     "/node_modules/",
     "^.+\\.module\\.(css|sass|scss)$",
   ],
   moduleNameMapper: {
-    "^.+\\.module\\.(css|sass|scss)$":
-      "<rootDir>/.jest/identity-obj-proxy-esm.js",
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     // Handle image imports
     // https://jestjs.io/docs/webpack#handling-static-assets
     "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": `<rootDir>/.jest/__mocks__/fileMock.js`,
