92ab190fafc506d72df84e72574f58f33471e9d5
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-04-12 12:49:50 +0300

    Warn dev when they import from react-i18next
    Most likely they want to be importing from next-i18next instead.
    
    Importing from react-i18next will yield a useTranslation hook that does
    not have access to translations. This may be hard to spot and debug.

--- src/tests/initI18n.ts
+++ src/tests/initI18n.ts
@@ -1,4 +1,5 @@
 import i18n from "i18next";
+// eslint-disable-next-line no-restricted-imports
 import { initReactI18next } from "react-i18next";
 
 import card from "../../public/locales/fi/card.json";
